393d29a38e115c21ec7483d04a40a743803237ba
Daniel DeGrasse <daniel.degrasse@nxp.com>
2024-05-23 14:41:25 -0500

    Generating FIRC clock outputs as child nodes of FIRC device
    This is needed because the FIRC_48M and FIRC_144M are really just
    clock gates for FIRC outputs
    
    Signed-off-by: Daniel DeGrasse <daniel.degrasse@nxp.com>

--- mcux/scripts/clocks/soc/mcxn9x.py
+++ mcux/scripts/clocks/soc/mcxn9x.py
@@ -27,38 +27,64 @@ def handle_soc_signals(peripheral_map, signal, level, proc_name):
     elif signal["id"] == "FIRC_48M" or signal["id"] == "FIRC_144M":
         # FIRC_48 and FIRC_144 are both "clock sources" that really act more
         # like gates, which source their clock frequency from the FIRC block
-        signal_xml = signal["source"]
-        enable_xml = signal_xml.find("configuration_element/item/assigns/assign")
-        bitfield = enable_xml.get("bit_field")
-        periph_reg = "SCG0::" + enable_xml.get("register")
-        (reg_addr, width, offset) = helpers.get_addr_and_bitfield(peripheral_map,
-                                                        periph_reg,
-                                                        bitfield)
-        if width != 1:
-            logging.warning("Clock source register %s has width >1", periph_reg)
-        # Now that we know register, write the node definition
-        dts = "\n"
-        dts += helpers.indent_string(f"{signal['id'].lower()}: ", level)
-        dts += f"{signal['id'].lower().replace('_','-')}@{reg_addr:x} {{\n"
-        dts += helpers.indent_string(f"compatible = \"nxp,mcxn9x-firc-gate\";\n", level + 1)
-        dts += helpers.indent_string(f"offset = <0x{offset:x}>;\n", level + 1)
-        dts += helpers.indent_string(f"#clock-cells = <1>;\n", level + 1)
-        dts += helpers.indent_string("parent = <&firc>;\n", level + 1)
-        # Write register name as comment
-        dts += helpers.indent_string(f"/* {periph_reg}[{bitfield}] */\n", level + 1)
-        dts += helpers.indent_string(f"reg = <0x{reg_addr:x} 0x{width:x}>;\n", level + 1)
-        dts += helpers.indent_string("};\n", level)
-        return dts
+        # We generate these nodes under the FIRC clock node, so skip generation
+        # here.
+        return "\n"
     elif signal["id"] == "PLL":
         # USB PLL
         return helpers.generate_node_with_compat(peripheral_map, signal, level,
                                                  proc_name, "nxp,mcxn9x-usb-pll",
                                                  1, "SCG0::SPLLCSR", None)
     elif signal["id"] == "FIRC":
-        # Fast Internal Reference Clock
-        return helpers.generate_node_with_compat(peripheral_map, signal, level,
-                                                 proc_name, "nxp,mcxn9x-firc",
-                                                 1, "SCG0::FIRCCSR", None)
+        # Fast Internal Reference Clock. We generate firc_48m and firc_144m
+        # as children of this node.
+        (reg_addr, width, _) = helpers.get_addr_and_bitfield(peripheral_map,
+                                                        "SCG0::FIRCCSR",
+                                                        None)
+        # Now that we know register, write the node definition
+        dts = "\n"
+        dts += helpers.indent_string(f"{signal['id'].lower()}: ", level)
+        dts += f"{signal['id'].lower().replace('_','-')}@{reg_addr:x} {{\n"
+        dts += helpers.indent_string(f"compatible = \"nxp,mcxn9x-firc\";\n", level + 1)
+        dts += helpers.indent_string(f"#clock-cells = <1>;\n", level + 1)
+        # Write register name as comment
+        dts += helpers.indent_string(f"/* SCG0::FIRCCSR */\n", level + 1)
+        dts += helpers.indent_string(f"reg = <0x{reg_addr:x} 0x{width:x}>;\n", level + 1)
+        dts += helpers.generate_children(peripheral_map, signal, level, proc_name)
+        # Hardcode FIRC_48M and FIRC_144M nodes. Both are clock gates,
+        # but FIRC_48M routes from a fixed divider of 3
+        dts += "\n"
+        dts += helpers.indent_string(f"firc_144m: firc-144m@{reg_addr:x} {{\n", level + 1)
+        dts += helpers.indent_string("compatible = \"nxp,syscon-clock-gate\";\n", level + 2)
+        dts += helpers.indent_string("#clock-cells = <1>;\n", level + 2)
+        dts += helpers.indent_string("/* SCG0::FIRCCSR[FIRC_FCLK_PERIPH_EN] */\n", level + 2)
+        (reg_addr, width, offset) = helpers.get_addr_and_bitfield(peripheral_map,
+                                                        "SCG0::FIRCCSR",
+                                                        "FIRC_FCLK_PERIPH_EN")
+        dts += helpers.indent_string(f"reg = <0x{reg_addr:x} 0x{width:x}>;\n", level + 2)
+        dts += helpers.indent_string(f"offset = <0x{offset:x}>;\n", level + 2)
+        dts += helpers.indent_string("};\n", level + 1)
+        dts += "\n"
+        dts += helpers.indent_string(f"firc_div3: firc-div3m {{\n", level + 1)
+        dts += helpers.indent_string("compatible = \"fixed-divider\";\n", level + 2)
+        dts += helpers.indent_string("#clock-cells = <0>;\n", level + 2)
+        dts += helpers.indent_string("divider = <3>;\n", level + 2)
+        dts += helpers.indent_string(f"#address-cells = <1>;\n", level + 2)
+        dts += helpers.indent_string(f"#size-cells = <1>;\n", level + 2)
+        dts += "\n"
+        dts += helpers.indent_string(f"firc_48m: firc-48m@{reg_addr:x} {{\n", level + 2)
+        dts += helpers.indent_string("compatible = \"nxp,syscon-clock-gate\";\n", level + 3)
+        dts += helpers.indent_string("#clock-cells = <1>;\n", level + 3)
+        dts += helpers.indent_string("/* SCG0::FIRCCSR[FIRC_SCLK_PERIPH_EN] */\n", level + 3)
+        (reg_addr, width, offset) = helpers.get_addr_and_bitfield(peripheral_map,
+                                                        "SCG0::FIRCCSR",
+                                                        "FIRC_SCLK_PERIPH_EN")
+        dts += helpers.indent_string(f"reg = <0x{reg_addr:x} 0x{width:x}>;\n", level + 3)
+        dts += helpers.indent_string(f"offset = <0x{offset:x}>;\n", level + 3)
+        dts += helpers.indent_string("};\n", level + 2)
+        dts += helpers.indent_string("};\n", level + 1)
+        dts += helpers.indent_string("};\n", level)
+        return dts
     elif signal["id"] == "PLL0":
         # APLL
         return helpers.generate_node_with_compat(peripheral_map, signal, level,
